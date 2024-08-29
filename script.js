89262474e2486e45df8fcf04164d2347b09caa6a
pepse3000 <113620477+pepse3000@users.noreply.github.com>
2024-08-30 00:01:41 +0300

    small css fixes, new logic in script.js, html fixes

--- script.js
+++ script.js
@@ -17,46 +17,137 @@ function divide (a, b) {
 function operate (operator, a, b) {
     switch(operator) {
         case "+":
-            add(a, b);
-            break;
+            return add(a, b);
         case "-":
-            substract(a, b);
-            break;
-        case "*":
-            multiply(a, b);
-            break;
+            return substract(a, b);
+        case "x":
+            return multiply(a, b);
         case "/":
-            divide(a, b);
-            break;
+            return divide(a, b);
     }
 }
 
 function startApp() {
-
+    addEventToEachButton();
 }
 
 function isNumber(btn) {
-    const numbers = "123456789"
+    const numbers = "0123456789"
     return numbers.includes(btn.textContent);
 }
 
 function addEventToEachButton () {
     const allButtons = document.querySelectorAll(".btn-input")
-    const numberButtons = Object.values(allButtons).filter(isNumber)
 
-    numberButtons.forEach((btn) => {
-        btn.addEventListener("click", () => appendNumber(btn.textContent))
+    allButtons.forEach((btn) => {
+        if (isNumber(btn)) {
+            btn.addEventListener("click", () => appendNumber(btn.textContent))
+        }
+
+        if (btn.textContent == "AC") {
+            btn.addEventListener("click", resetOutput)
+        }
+
+        if (btn.textContent == "Del") {
+            btn.addEventListener("click", deleteNumber)
+        }
+
+        if (btn.textContent == ".") {
+            btn.addEventListener("click", addComma)
+        }
+
+        if (btn.textContent == "+") {
+            btn.addEventListener("click", () => addOperation("+"))
+        }
+
+        if (btn.textContent == "x") {
+            btn.addEventListener("click", () => addOperation("x"))
+        }
+
+        if (btn.textContent == "=") {
+            btn.addEventListener("click", submitOperate)
+        }
     })
-
-    return numberButtons
 }
 
 function appendNumber (number) {
-    output = document.querySelector(".current-output");
-
-    if (output.textContent) {
+    if (output.textContent && output.textContent != "0") {
         output.textContent += number;
     } else {
         output.textContent = number;
     }
+
 }
+
+function resetOutput () {
+    output.textContent = "0"
+    memoryOutput.textContent = "";
+}
+
+function deleteNumber () {
+    if (output.textContent.length <= 1) {
+        output.textContent = 0;
+        return;
+    }
+
+    output.textContent = output.textContent.slice(0, output.textContent.length - 1);
+}
+
+function addComma () {
+    if (output.textContent.includes(".")) {
+        return;
+    } else {
+        output.textContent += ".";
+    }
+}
+
+function addOperation (operation) {
+    memoryOutput.textContent = output.textContent + ` ${operation} `;
+    output.textContent = "0";
+}
+
+// Gets directly variables from the transaction history and/or from the current input. Performs the desired operations
+function submitOperate () {
+    if (output.textContent == "0" || memoryOutput.textContent == "") {
+        return;
+    }
+
+    const input = memoryOutput.textContent.split(" ")
+    let firstNumber = input[0];
+    const operation = input[1];
+    let secondNumber = 0;
+
+    if (input[2] == "") {
+        secondNumber = output.textContent;
+    } else {
+        firstNumber = output.textContent;
+        secondNumber = input[2];
+    }
+    
+    let result = operate(operation, firstNumber, secondNumber);
+    
+    let floatResult = checkFloat(result);
+
+    output.textContent = floatResult;
+    memoryOutput.textContent = `${firstNumber} ${operation} ${secondNumber} =`;
+}
+
+// Checks the number of zeros after the decimal point. If more than 5, rounds up
+function checkFloat(result) {
+    const splittedResult = result.toString().split("");
+    if (splittedResult.includes(".")) {
+        const floatArr = splittedResult.slice(splittedResult.indexOf(".") + 1);
+        console.log(floatArr)
+        if (floatArr.length > 5) {
+            return +result.toFixed(5);
+        }
+        
+    }
+    return result;
+}
+
+output = document.querySelector(".current-output");
+memoryOutput = document.querySelector(".memory-output")
+
+// App Init
+startApp();
